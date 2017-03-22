68add93385e414fb2fd3aa4e960ee98c87a3d440
Your Name <you@example.com>
2017-03-22 15:00:40 +1100

    question3

--- kern/compile/ASST1/.depend.producerconsumer.c
+++
@@ -1,3 +0,0 @@
-producerconsumer.o: ../../asst1/producerconsumer.c ../../include/types.h \
- ../../include/kern/types.h includelinks/kern/machine/types.h \
- includelinks/machine/types.h ../../asst1/producerconsumer_driver.h
